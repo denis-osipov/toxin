ebb04c371cab35c0075712c75663513ccc64f0d5
Denis Osipov <osipov_d@list.ru>
2019-07-30 17:46:42 +0500

    Add file-loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|svg|jpg|gif)$/,
+        use:[
+          'file-loader'
+        ]
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader',
