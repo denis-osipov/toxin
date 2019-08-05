3c3cfee7099f1d161bbb28491f8937a8c519bf61
Denis Osipov <osipov_d@list.ru>
2019-08-05 10:36:04 +0500

    Set entries generation for watching

--- generator/EntryGenerationPlugin.js
+++ generator/EntryGenerationPlugin.js
@@ -1,11 +1,29 @@
+// Plugin for generation entry points
+
+const path = require('path');
+
 // Load main functionality
-const generator = require('./generateEntries')
+const generator = require('./generateEntries');
 
 class EntryGenerationPlugin {
   apply(compiler) {
     compiler.hooks.entryOption.tap(
       'EntryGenerationPlugin',
       generator
+    );
+
+    compiler.hooks.invalid.tap(
+      'EntryGenerationPlugin',
+      (fileName, changeTime) => {
+        // Avoid infinite generation loop
+        for (let entry in Object.values(compiler.options.entry)) {
+          entry = typeof entry === 'string' ? entry : entry[1];
+          if (fileName === path.resolve(compiler.context, entry)) {
+            return;
+          }
+        }
+        generator(compiler.context, compiler.options.entry);
+      }
     );
   }
 }
