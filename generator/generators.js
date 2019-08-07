d8d60adf9268904f69d5a29999e159593bca125b
Denis Osipov <osipov_d@list.ru>
2019-08-07 15:07:11 +0500

    Generate scss files

--- generator/generators.js
+++ generator/generators.js
@@ -3,38 +3,77 @@
 const path = require('path');
 const fs = require('fs');
 
+// Global rules for file types
+const rules = {
+  js: {
+    message: '// File generated automatically.\n// Any changes will be discarded during next compilation.\n\n',
+    prepend: [],
+    use: function(entityFile, entryFile) {
+      return `import '${path.relative(path.dirname(entryFile), entityFile).replace(/\\/g, '/')}';\n`;
+    }
+  },
+  scss: {
+    message: '// File generated automatically.\n// Any changes will be discarded during next compilation.\n\n',
+    prepend: [
+      `@import 'fonts/fonts';\n`,
+      `@import 'colors/colors.scss';\n`,
+      `@import 'mixins';\n`
+    ],
+    use: function(entityFile) {
+      const entity = path.basename(entityFile, '.scss');
+      return `@include ${entity};\n`;
+    }
+  },
+  pug: {
+    message: '//- File generated automatically.\n//- Any changes will be discarded during next compilation.\n\n'
+  }
+};
+
 // Generate entries js files
 function generateEntries(context, entry) {
   const blocksPath = path.join(context, 'blocks');
   for (point in entry) {
-    let entryPath;
-    if (typeof entry[point] === "string") {
-      entryPath = path.resolve(context, entry[point]);
-    }
-    else {
-      entryPath = path.resolve(context, entry[point][1]);
-    }
-    const templatePath = entryPath.replace('.js', '.pug');
-    const stylePath = entryPath.replace('.js', '.scss');
 
-    const templateContent = fs.readFileSync(templatePath, 'utf-8');
-    const bems = getBemList(templateContent);
+    let entryFiles = getFileList(entry[point], context);
 
-    let entryContent = '// File generated automatically.\n// Any changes will be discarded during next compilation.\n\n';
+    // Get list of used bem entities
+    const templateFile = entryFiles[0].replace(/\.(js|scss)$/, '.pug');
+    const templateContent = fs.readFileSync(templateFile, 'utf-8');
+    const usedBems = getBemList(templateContent);
 
-    // Import page styles, if exist
-    if (fs.existsSync(stylePath)) {
-      entryContent += `import './${path.basename(stylePath)}';\n`;
-    }
+    // Write files
+    entryFiles.forEach(function(file) {
+      const type = path.extname(file).slice(1);
+      fs.writeFileSync(file, rules[type].message); // warning about autogeneration
 
-    bems.forEach(function(entity) {
-      const entityPath = path.join(blocksPath, entity, entity + '.js');
-      if (fs.existsSync(entityPath)) {
-        entryContent += `import '${path.relative(path.dirname(entryPath), entityPath).replace(/\\/g, '/')}';\n`;
-      }
+      // Mandatory imports
+      rules[type].prepend.forEach(function(row) {
+        fs.appendFileSync(file, row, 'utf-8');
+      });
+
+      // Add used bem entities
+      usedBems.forEach(function(entity) {
+        const entityFile = path.join(blocksPath, entity, entity + `.${type}`);
+        if (fs.existsSync(entityFile)) {
+          fs.appendFileSync(file, rules[type].use(entityFile, file), 'utf-8');
+        }
+      });
     });
-    fs.writeFileSync(entryPath, entryContent);
   }
+}
+
+function getFileList(entryPoint, context) {
+  let entryFiles = [];
+  if (typeof entryPoint === "string") {
+    entryFiles.push(path.resolve(context, entryPoint));
+  }
+  else {
+    const filteredPaths = entryPoint.filter(entryPath => !entryPath.includes('node_modules'));
+    filteredPaths.forEach(entryPath => {
+      entryFiles.push(path.resolve(context, entryPath));
+    });
+  }
+  return entryFiles;
 }
 
 // Get bem list from pug template (bem entities added with +bem('name'))
@@ -52,10 +91,10 @@ function getBemList(data) {
 function aggregateMixins(context) {
   const blocksPath = path.join(context, 'blocks');
 
+  // Rules for mixins aggregation
   let bems = {
     pug: {
       file: path.join(blocksPath, 'mixins.pug'),
-      message: '//- File generated automatically.\n//- Any changes will be discarded during next compilation.\n',
       include: function(entityPath) {
         return `include /${path.relative(blocksPath, entityPath).replace(/\\/g, '/')}\n`;
       },
@@ -63,7 +102,6 @@ function aggregateMixins(context) {
     },
     scss: {
       file: path.join(blocksPath, 'mixins.scss'),
-      message: '// File generated automatically.\n// Any changes will be discarded during next compilation.\n',
       include: function(entityPath) {
         return `@import '${path.relative(blocksPath, entityPath).replace(/\\/g, '/')}';\n`;
       },
@@ -71,27 +109,31 @@ function aggregateMixins(context) {
     }
   };
 
+  fillBemFilesList(blocksPath, { '.pug': bems.pug.fileList, '.scss': bems.scss.fileList });
+
+  // Write files
   for (type in bems) {
-    getBemFilesList(blocksPath, bems[type].fileList, type);
-    fs.writeFileSync(bems[type].file, bems[type].message);
+    fs.writeFileSync(bems[type].file, rules[type].message);
     bems[type].fileList.forEach(function(entityPath) {
       fs.appendFileSync(bems[type].file, bems[type].include(entityPath), 'utf-8');
     });
   }
 }
 
-// Get bem files list
-function getBemFilesList(root, list, type) {
+// Fill bem files list
+function fillBemFilesList(root, lists) {
   const blocks = fs.readdirSync(root, { encoding: 'utf-8', withFileTypes: true });
   blocks.forEach(function(entity){
     const entityPath = path.join(root, entity.name);
-    if (entity.isFile() && entity.name.endsWith(`.${type}`) && entity.name !== `mixins.${type}`) {
-      list.push(entityPath);
+    if (entity.isFile()) {
+      if (path.extname(entity.name) in lists && path.parse(entity.name).name !== 'mixins') {
+        lists[path.extname(entity.name)].push(entityPath);
+      }
     }
     else if (entity.isDirectory()) {
-      getBemFilesList(entityPath, list, type);
+      fillBemFilesList(entityPath, lists);
     }
-  })
+  });
 }
 
-module.exports = { aggregateMixins, generateEntries }
+module.exports = { aggregateMixins, generateEntries, getFileList }
