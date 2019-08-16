1946a6dc5597e7c21af35c0d074a4ec132a9536d
Denis Osipov <osipov_d@list.ru>
2019-08-16 12:55:02 +0500

    Refactor dropdown

--- src/blocks/dropdown/_type/dropdown_type_guests.js
+++ src/blocks/dropdown/_type/dropdown_type_guests.js
@@ -1,25 +1,23 @@
 function setGuests( jQuery ) {
   $( '.dropdown_type_guests' ).dropdown({
-    wording: [
-      (value) => {
-        if (value == 0) {
-          return 'Сколько гостей';
-        }
-        else if (value === 1 || (value % 100 !== 11 && value % 10 === 1)) {
-          return value + ' гость';
-        }
-        else if ((function() {
-          const remMain = value % 100;
-          const remSub = remMain % 10;
-          return remSub > 1 && remSub < 5 && !(remMain > 11 && remMain < 15);
-        })()) {
-          return value + ' гостя';
-        }
-        else {
-          return value + ' гостей';
-        }
+    wording: (value) => {
+      if (value == 0) {
+        return 'Сколько гостей';
       }
-    ]
+      else if (value === 1 || (value % 100 !== 11 && value % 10 === 1)) {
+        return value + ' гость';
+      }
+      else if ((function() {
+        const remMain = value % 100;
+        const remSub = remMain % 10;
+        return remSub > 1 && remSub < 5 && !(remMain > 11 && remMain < 15);
+      })()) {
+        return value + ' гостя';
+      }
+      else {
+        return value + ' гостей';
+      }
+    }
   });
 }
 
--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -4,76 +4,98 @@ import './_type/dropdown_type_conveniences';
 (function( $ ) {
   $.fn.dropdown = function( options ) {
 
-    let settings = $.extend({
-      total: true,
-      sep: ', ',
-      wording: [
-        (value) => {
-          if (value === 1) {
-            return value + ' item';
-          }
-          else {
-            return value + ' items';
-          }
-        }
-      ]
-    }, options);
+    const settings = $.extend( {}, $.fn.dropdown.defaults, options);
 
     return this.each(function() {
 
-      function update() {
-        if (settings.total) {
-          const total = values.reduce((prev, current) => prev + current);
-          input.val(settings.wording[0](total));
-        }
-        else {
-          let totals = [];
-          settings.wording.forEach((fun, index) => {
-            totals.push(fun(values[index]));
+      let dropdown = {
+        dropdown: $( this ),
+        init: function() {
+          dropdown.settings = settings;
+          dropdown.update = $.fn.dropdown.update;
+          dropdown.expand = $.fn.dropdown.expand;
+          dropdown.change = $.fn.dropdown.change.bind(this);
+          dropdown.setElements();
+          dropdown.setValues();
+        },
+        setElements: function() {
+          dropdown.input = dropdown.dropdown.find('.field');
+          dropdown.items = dropdown.dropdown.find('.dropdown__item');
+          dropdown.total = dropdown.dropdown.find('.dropdown__total');
+          dropdown.list = dropdown.dropdown.find('.dropdown__list');
+        },
+        setValues: function() {
+          dropdown.values = [];
+          dropdown.items.each(function(index, element) {
+            const valueField = $( element ).find('.dropdown__item-value');
+            dropdown.values.push(parseInt(valueField.text()));
           });
-          input.val(totals.join(settings.sep));
         }
       }
+      dropdown.init();
+      dropdown.update();
 
-      const $dropdown = $( this );
-      const input = $dropdown.find('.field');
-      let values = [];
-      const items = $dropdown.find('.dropdown__item');
-      items.each(function(index, element) {
-        const valueField = $( element ).find('.dropdown__item-value');
-        values.push(parseInt(valueField.text()));
-      });
-
-      update();
-
-      $dropdown.find('.dropdown__total').on('click', function(event) {
-        $dropdown.find('.dropdown__list').toggleClass('dropdown__list_hidden');
-      });
-
-      items.on('click', '.button', function(event) {
-        const button = $( this );
-        const item = $( event.delegateTarget );
-        const index = item.index();
-        const valueField = item.find('.dropdown__item-value');
-        if (button.hasClass('dropdown__button_type_decrement')) {
-          values[index] -= 1;
-          if (values[index] < 1) {
-            button.prop('disabled', true);
-          }
-        }
-        else {
-          values[index] += 1;
-          const decrButton = item.find('.dropdown__button_type_decrement');
-          if (decrButton.prop('disabled')) {
-            decrButton.prop('disabled', false);
-          };
-        }
-        valueField.text(values[index]);
-
-        update();
-      });
+      dropdown.total.on('click', dropdown.expand);
+      dropdown.items.on('click', '.button', dropdown.change);
     });
-  }
+  };
+
+  $.fn.dropdown.update = function () {
+    if (this.settings.total) {
+      const total = this.values.reduce((prev, current) => prev + current);
+      this.input.val(this.settings.wording(total));
+    }
+    else {
+      let totals = [];
+      this.settings.wording.forEach((fun, index) => {
+        totals.push(fun(this.values[index]));
+      });
+      this.input.val(totals.join(this.settings.sep));
+    }
+  };
+
+  $.fn.dropdown.expand = function(event) {
+    $( event.currentTarget ).siblings('.dropdown__list').toggleClass('dropdown__list_hidden');
+  };
+
+  $.fn.dropdown.change = function(event) {
+    const button = $( event.target );
+    const item = $( event.delegateTarget );
+    const index = item.index();
+    const valueField = item.find('.dropdown__item-value');
+    if (button.hasClass('dropdown__button_type_decrement')) {
+      this.values[index] -= 1;
+      if (this.values[index] < 1) {
+        button.prop('disabled', true);
+      }
+    }
+    else {
+      this.values[index] += 1;
+      const decrButton = item.find('.dropdown__button_type_decrement');
+      if (decrButton.prop('disabled')) {
+        decrButton.prop('disabled', false);
+      };
+    }
+    valueField.text(this.values[index]);
+
+    this.update();
+  };
+
+  $.fn.dropdown.format = function(value) {
+    if (value === 1) {
+      return value + ' item';
+    }
+    else {
+      return value + ' items';
+    }
+  };
+
+  $.fn.dropdown.defaults = {
+    total: true,
+    sep: ', ',
+    wording: $.fn.dropdown.format
+  };
+
 })( jQuery );
 
 function setDropdown( jQuery ) {
