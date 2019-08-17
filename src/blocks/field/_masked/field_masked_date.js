46b92a1adf1428cd3c8d1af848fbb9cc02daf2cb
Denis Osipov <osipov_d@list.ru>
2019-08-17 12:21:05 +0500

    Tune masked field

--- src/blocks/field/_masked/field_masked_date.js
+++ src/blocks/field/_masked/field_masked_date.js
@@ -2,7 +2,12 @@ import 'inputmask/dist/inputmask/inputmask.date.extensions';
 import 'inputmask/dist/inputmask/jquery.inputmask';
 
 function maskInput( jQuery ) {
-  $( '.field_masked_date' ).inputmask('datetime', {inputFormat: 'dd.mm.yyyy'});
+  $( '.field_masked_date' ).inputmask('datetime', {
+    placeholder: 'ДД.ММ.ГГГГ',
+    inputFormat: 'dd.mm.yyyy',
+    showMaskOnFocus: false,
+    showMaskOnHover: false
+  });
 }
 
 $( document ).ready( maskInput );
