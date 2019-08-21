0c0ae224cf303793a0fcd34a135b577895ed6921
Denis Osipov <osipov_d@list.ru>
2019-08-21 16:10:19 +0500

    Add more dropdowns to form-elements

--- src/pages/form-elements/page-script.js
+++ src/pages/form-elements/page-script.js
@@ -1,8 +1,26 @@
 $( document ).ready(function(jQery) {
-  const secondDateField = $( '.date-picker' ).eq(1).find('.field');
-  secondDateField.data('datepicker').selectDate(new Date(2019, 7, 19));
-  secondDateField.data('datepicker').selectedDates = [];
+  const secondDatepicker = $( '.date-picker' ).eq(1).find('.field').data('datepicker');
+  secondDatepicker.selectDate(new Date(2019, 7, 19));
+  secondDatepicker.selectedDates = [];
 
-  const thirdDateField = $( '.date-picker' ).eq(2).find('.field');
-  thirdDateField.data('datepicker').selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+  const thirdDatepicker = $( '.date-picker' ).eq(2).find('.field').data('datepicker');
+  thirdDatepicker.selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+
+  const secondConvDropdown = $( '.dropdown_type_conveniences' ).eq(1).data('dropdown');
+  secondConvDropdown.items.each(function(index, element) {
+    if (index < 2) {
+      $( element ).find('.dropdown__item-value').text(2);
+      $( element ).find('.dropdown__button_type_decrement').prop('disabled', false);
+    }
+  });
+  secondConvDropdown.update();
+  secondConvDropdown.expand();
+
+  const thirdGuestDropdown = $( '.dropdown_type_guests' ).eq(2).data('dropdown');
+  thirdGuestDropdown.items.eq(0).find('.dropdown__item-value').text(2);
+  thirdGuestDropdown.items.eq(0).find('.dropdown__button_type_decrement').prop('disabled', false);
+  thirdGuestDropdown.items.eq(1).find('.dropdown__item-value').text(1);
+  thirdGuestDropdown.items.eq(1).find('.dropdown__button_type_decrement').prop('disabled', false);
+  thirdGuestDropdown.update();
+  thirdGuestDropdown.expand();
 });
