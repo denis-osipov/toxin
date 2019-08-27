b59369c9e8983aabe2b1a667f3872ae57501c993
Denis Osipov <osipov_d@list.ru>
2019-08-27 16:19:11 +0500

    Rewrite slider

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,16 +1,26 @@
 import 'ion-rangeslider';
 
 function setSlider( jQuery ) {
-  $(".slider").ionRangeSlider({
-    skin: 'round',
+  $(".slider__input").ionRangeSlider({
+    skin: 'toxin',
     type: 'double',
-    min: 0,
-    max: 15000,
+    min: 600,
+    max: 15300,
     from: 5000,
     to: 10000,
     hide_min_max: true,
-    postfix: '₽'
+    hide_from_to: true,
+    postfix: '₽',
+    values_separator: ' - ',
+    onStart: output,
+    onChange: output
   });
+}
+
+function output(data) {
+  const output = $( data.input ).parent().find('.slider__output');
+  const value = `${data['from_pretty']}${this.postfix}${this['values_separator']}${data['to_pretty']}${this.postfix}`;
+  output.text(value);
 }
 
 $( document ).ready( setSlider );
