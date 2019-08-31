7bfaacd1591f9af89afff4684b901c28dea13e3d
Denis Osipov <osipov_d@list.ru>
2019-08-31 14:26:25 +0500

    Use input element in like block

--- src/blocks/like/like.js
+++ src/blocks/like/like.js
@@ -1,5 +1,8 @@
 function setLike( jQuery ) {
   $( '.like' ).each(function(index, element) {
+    $( element ).find('.like__input').on('click', function(event) {
+      event.stopPropagation();
+    });
     $( element ).on('click', function(event) {
       const counElement = $( element ).find('.like__count');
       let count = parseInt(counElement.text());
