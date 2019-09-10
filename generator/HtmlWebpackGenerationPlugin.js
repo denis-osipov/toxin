5483e2abad74de1a7817a3b4fd70fe4382074dd3
Denis Osipov <osipov_d@list.ru>
2019-09-10 13:49:02 +0500

    Try to use runLoaders

--- generator/HtmlWebpackGenerationPlugin.js
+++ generator/HtmlWebpackGenerationPlugin.js
@@ -1,15 +1,29 @@
-// If your plugin is direct dependent to the html webpack plugin:
+const runLoaders = require("loader-runner").runLoaders;
 
 class HtmlWebpackGenerationPlugin {
   apply (compiler) {
     compiler.hooks.compilation.tap(
       'HtmlWebpackGenerationPlugin',
       (compilation, compilationParams) => {
+        let loaderContext = {};
+        compilation.hooks.normalModuleLoader.call(loaderContext);
+        let template;
+        runLoaders({
+          resource: 'D:/fsd/toxin/src/pages/colors-and-type/colors-and-type.pug',
+          loaders: [{
+            loader: 'D:/fsd/toxin/node_modules/pug-loader/index.js',
+            options: {
+              root: 'D:/fsd/toxin/src/blocks',
+            }
+          }],
+          context: loaderContext
+        }, (err, result) => {
+          template = result;
+        });
 
-        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+        compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration.tapAsync(
           'HtmlWebpackGenerationPlugin',
           (pluginArgs, callback) => {
-            console.log(pluginArgs);
             callback();
           });
 
