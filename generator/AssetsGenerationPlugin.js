e0602a6dfcf24d494288978cf6c8d0cf8460768b
Denis Osipov <osipov_d@list.ru>
2019-09-15 00:58:42 +0500

    Change plugin for new generation concept

--- generator/AssetsGenerationPlugin.js
+++ generator/AssetsGenerationPlugin.js
@@ -1,19 +1,30 @@
 // Plugin for generation of entry points and aggregation of mixins
 
 const path = require('path');
-const generator = require('./generators').generateEntries;
-const aggregator = require('./generators').aggregateMixins;
-const getFileList = require('./generators').getFileList;
+const generator = require('./generator');
 
 class AssetsGenerationPlugin {
+  constructor(options) {
+    this.options = Object.assign({
+    }, options);
+  }
+
   apply(compiler) {
 
     // Generate at startup
     compiler.hooks.entryOption.tap(
       'AssetsGenerationPlugin',
       (context, entry) => {
-        aggregator(context);
-        generator(context, entry);
+        if (!this.options.blocksFolder) {
+          this.options.blocksFolder = path.resolve(context, 'blocks');
+        }
+        if (!this.options.pagesFolders) {
+          this.options.pagesFolders = getFolders(context, entry);
+        }
+        else if (typeof this.options.pagesFolders === 'string') {
+          this.options.pagesFolders = [this.options.pagesFolders];
+        }
+        generator(this.options.blocksFolder, this.options.pagesFolders);
       }
     );
 
@@ -23,27 +34,39 @@ class AssetsGenerationPlugin {
       (fileName, changeTime) => {
 
         // Don't respond to changes of generated files
-        if (
-          fileName === path.resolve(compiler.context, 'blocks/mixins.pug') ||
-          fileName === path.resolve(compiler.context, 'blocks/mixins.scss')
-        ) {
+        if (fileName.includes('dependency.')) {
           return;
         }
-
-        for (let entryPoint of Object.values(compiler.options.entry)) {
-          let entryFiles = getFileList(entryPoint, compiler.context);
-          for (let file of entryFiles) {
-            // Check if invalid file is one of generated
-            if (fileName === file) {
-              return;
-            }
-          }
-        }
-        aggregator(compiler.context);
-        generator(compiler.context, compiler.options.entry);
+        generator(this.options.blocksFolder, this.options.pagesFolders);
       }
     );
   }
+}
+
+function getFolders(context, entry) {
+  const entryDirs = new Set();
+  if (typeof entry === "string") {
+    entryDirs.add(path.resolve(context, path.dirname(entry)));
+  }
+  else if (Array.isArray(entry)) {
+    entry.forEach(file => {
+      entryDirs.add(path.resolve(context, path.dirname(file)));
+    });
+  }
+  else {
+    for (point of Object.entries(entry)) {
+      const [name, files] = point;
+      if (typeof files === "string") {
+        entryDirs.add(path.resolve(context, path.dirname(files)));
+      }
+      else {
+        files.forEach(file => {
+          entryDirs.add(path.resolve(context, path.dirname(file)));
+        });
+      }
+    }
+  }
+  return Array.from(entryDirs);
 }
 
 module.exports = AssetsGenerationPlugin;
