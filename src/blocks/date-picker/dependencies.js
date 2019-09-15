7a66032ef2338dbc9f16d2ab090516b4b00df6df
Denis Osipov <osipov_d@list.ru>
2019-09-15 23:47:36 +0500

    Generate dependencies

---
+++ src/blocks/date-picker/dependencies.js
@@ -0,0 +1,4 @@
+// File generated automatically.
+// Any changes will be discarded during next compilation.
+
+import '__field/date-picker__field.js';
---
+++ src/pages/form-elements/dependencies.js
@@ -0,0 +1,10 @@
+// File generated automatically.
+// Any changes will be discarded during next compilation.
+
+import '../../blocks/input/input.js';
+import '../../blocks/dropdown/dropdown.js';
+import '../../blocks/date-picker/date-picker.js';
+import '../../blocks/like/like.js';
+import '../../blocks/slider/slider.js';
+import '../../blocks/exp-list/exp-list.js';
+import '../ui-kit/ui-kit.pug';
