da8626ec776bb39f0e65e493c625831d2992c748
Denis Osipov <osipov_d@list.ru>
2019-09-28 04:16:36 +0500

    Add user-info block

--- src/blocks/user-info/dependencies.js
+++ src/blocks/user-info/dependencies.js
@@ -1,6 +1,5 @@
 // File generated automatically.
 // Any changes will be discarded during next compilation.
 
-import '../user-info/user-info.js';
 import '../input/input.js';
-import '../checkbox/checkbox.js';
+import '../list/list.js';
