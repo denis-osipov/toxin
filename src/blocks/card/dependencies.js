e311c73926d1a0b0ede06ae9e3563ba2de162c75
Denis Osipov <osipov_d@list.ru>
2019-09-28 03:08:12 +0500

    Add card block

---
+++ src/blocks/card/dependencies.js
@@ -0,0 +1,7 @@
+// File generated automatically.
+// Any changes will be discarded during next compilation.
+
+import '../search/search.js';
+import '../registration/registration.js';
+import '../reservation/reservation.js';
+import '../login/login.js';
--- src/pages/cards/dependencies.js
+++ src/pages/cards/dependencies.js
@@ -1,7 +1,4 @@
 // File generated automatically.
 // Any changes will be discarded during next compilation.
 
-import '../../blocks/search/search.js';
-import '../../blocks/registration/registration.js';
-import '../../blocks/reservation/reservation.js';
-import '../../blocks/login/login.js';
+import '../../blocks/card/card.js';
