8d0976ca47eb7be54e29d12eea34dcab4b62c7ff
Denis Osipov <osipov_d@list.ru>
2019-09-28 16:06:01 +0500

    Add account block

--- src/blocks/account/dependencies.js
+++ src/blocks/account/dependencies.js
@@ -2,6 +2,3 @@
 // Any changes will be discarded during next compilation.
 
 import '../input/input.js';
-import '../list/list.js';
-import '../account/account.js';
-import '../checkbox/checkbox.js';
