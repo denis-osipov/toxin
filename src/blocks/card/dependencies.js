078fbc262d943429fa0981a692601cf25657e3ba
Denis Osipov <osipov_d@list.ru>
2019-09-30 03:10:45 +0500

    Rewrite reservation as element of cards block

--- src/blocks/card/dependencies.js
+++ src/blocks/card/dependencies.js
@@ -2,4 +2,5 @@
 // Any changes will be discarded during next compilation.
 
 import './__registration/card__registration.js';
+import './__reservation/card__reservation.js';
 import './__search/card__search.js';
