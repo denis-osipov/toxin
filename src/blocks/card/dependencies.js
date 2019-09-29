c22e12af6ee01459a33242d4308e4df476090017
Denis Osipov <osipov_d@list.ru>
2019-09-29 14:56:19 +0500

    Rewrite search block as element in card block

--- src/blocks/card/dependencies.js
+++ src/blocks/card/dependencies.js
@@ -1,7 +1,4 @@
 // File generated automatically.
 // Any changes will be discarded during next compilation.
 
-import '../search/search.js';
-import '../registration/registration.js';
-import '../reservation/reservation.js';
-import '../login/login.js';
+import './__search/card__search.js';
