4342d570d00b2154013efe0f726ac8b7ccb1dd74
Denis Osipov <osipov_d@list.ru>
2019-09-29 15:07:42 +0500

    Rewrite registration block as element in card block

---
+++ src/blocks/card/__registration/dependencies.js
@@ -0,0 +1,7 @@
+// File generated automatically.
+// Any changes will be discarded during next compilation.
+
+import '../../input/input.js';
+import '../../list/list.js';
+import '../../account/account.js';
+import '../../checkbox/checkbox.js';
--- src/blocks/card/dependencies.js
+++ src/blocks/card/dependencies.js
@@ -1,4 +1,5 @@
 // File generated automatically.
 // Any changes will be discarded during next compilation.
 
+import './__registration/card__registration.js';
 import './__search/card__search.js';
