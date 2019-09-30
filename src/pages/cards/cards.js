93d742bd44e1e75e18cdedaad967f1ca4f7d1656
Denis Osipov <osipov_d@list.ru>
2019-09-30 15:50:48 +0500

    Add script for cards page

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,1 +1,11 @@
 import './dependencies.js';
+$( document ).ready(function(jQery) {
+
+  const reservationIn = $( '.card_reservation' ).find('.date-picker').eq(0).find('.input').data('datepicker');
+  reservationIn.selectDate(new Date(2019, 7, 19));
+  const reservationOut = $( '.card_reservation' ).find('.date-picker').eq(1).find('.input').data('datepicker');
+  reservationOut.selectDate(new Date(2019, 7, 23));
+
+  const reservationGuests = $( '.card_reservation' ).find('.dropdown_type_guests').data('dropdown');
+  reservationGuests.set([2, 1]);
+});
