1dffe581c0a013722c6876b69785aa1a6df8e486
Denis Osipov <osipov_d@list.ru>
2019-10-10 17:03:55 +0500

    Comment out cards script

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,18 +1,18 @@
 import './dependencies.js';
 $( document ).ready(function(jQery) {
 
-  const reservationIn = $( '.card_reservation' ).find('.date-picker').eq(0).find('.input').data('datepicker');
-  reservationIn.selectDate(new Date(2019, 7, 19));
-  const reservationOut = $( '.card_reservation' ).find('.date-picker').eq(1).find('.input').data('datepicker');
-  reservationOut.selectDate(new Date(2019, 7, 23));
+  // const reservationIn = $( '.card_reservation' ).find('.date-picker').eq(0).find('.input').data('datepicker');
+  // reservationIn.selectDate(new Date(2019, 7, 19));
+  // const reservationOut = $( '.card_reservation' ).find('.date-picker').eq(1).find('.input').data('datepicker');
+  // reservationOut.selectDate(new Date(2019, 7, 23));
 
-  const reservationGuests = $( '.card_reservation' ).find('.dropdown_type_guests').data('dropdown');
-  reservationGuests.set([2, 1]);
+  // const reservationGuests = $( '.card_reservation' ).find('.dropdown_type_guests').data('dropdown');
+  // reservationGuests.set([2, 1]);
 
-  const calendar = $( '.calendar__picker' ).data('datepicker');
-  calendar.date = new Date(2019, 7, 8);
-  calendar.selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
-  calendar.$el.find('[data-date="8"]').addClass('-current-');
+  // const calendar = $( '.calendar__picker' ).data('datepicker');
+  // calendar.date = new Date(2019, 7, 8);
+  // calendar.selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+  // calendar.$el.find('[data-date="8"]').addClass('-current-');
 
-  $( '.room-plate__arrows' ).eq(1).hide();
+  // $( '.room-plate__arrows' ).eq(1).hide();
 });
