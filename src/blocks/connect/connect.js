62d1c0c3c671984bd212db809d4ba7f9db3be23b
Denis Osipov <osipov_d@list.ru>
2019-10-13 15:27:16 +0500

    Add functions to connect

--- src/blocks/connect/connect.js
+++ src/blocks/connect/connect.js
@@ -18,4 +18,24 @@ function connect(target, controlSelector, readyParams, initAction) {
   target.trigger('target:ready', readyParams);
 }
 
-module.exports = connect;
+function toDashString(string) {
+  const chars = Array.from(string);
+  chars.forEach((char, index, array) => {
+    if (char === char.toUpperCase()) {
+      array[index] = `-${char.toLowerCase()}`;
+    }
+  });
+  return chars.join('');
+}
+
+// Converts object keys to data-attrs
+function toDataAttrs(object) {
+  const convObject = {};
+  Object.entries(object).forEach(entry => {
+    const [name, value] = entry;
+    convObject[`data-${toDashString(name)}`] = value;
+  });
+  return convObject;
+}
+
+module.exports = { connect, toDataAttrs };
