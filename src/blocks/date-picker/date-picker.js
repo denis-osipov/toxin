8fe70ba0b6f62442e8578f110b0358ff7cee0e60
Denis Osipov <osipov_d@list.ru>
2019-10-13 15:32:56 +0500

    Improve date-picker (use calendar and expand double modifier)

--- src/blocks/date-picker/date-picker.js
+++ src/blocks/date-picker/date-picker.js
@@ -1,18 +1,63 @@
 import './dependencies.js';
-const connect = require('blocksPath/connect/connect');
+
+(function( $ ) {
+  $.fn.toxinDatePicker = function() {
+    return this.each(function() {
+      const picker = {
+        container: $( this ),
+        fields: $( this ).find('.date-picker__field'),
+        inputs: $( this ).find('.input'),
+        calendar: $( this ).find('.date-picker__calendar'),
+        type: $( this ).hasClass('date-picker_double') ? 'double' : 'single',
+        months: $.fn.toxinDatePicker.months,
+        setDate: (dates) => {
+          if (dates[0] === '') {
+            $( picker.inputs ).val('');
+          }
+          else if (picker.type === 'single') {
+            const formattedDates = [];
+            dates.forEach(date => {
+              formattedDates.push(`${date.getDate()} ${picker.months[date.getMonth()]}`);
+            });
+            $( picker.inputs[0] ).val(formattedDates.join(' - '));
+          }
+          else {
+            dates.forEach((date, index) => {
+              $( picker.inputs[index] ).val(date.toLocaleDateString());
+            });
+          }
+        }
+      }
+  
+      picker.fields.on('click', function(event) {
+        picker.calendar.toggle();
+      });
+      
+      picker.container.on('calendar:date', function(event, ...dates) {
+        picker.setDate(dates);
+      });
+    });
+  };
+
+  $.fn.toxinDatePicker.months = [
+    'янв',
+    'фев',
+    'мар',
+    'апр',
+    'май',
+    'июн',
+    'июл',
+    'авг',
+    'сен',
+    'окт',
+    'ноя',
+    'дек'
+  ];
+})( jQuery );
 
 function setDatepicker( jQuery ) {
 
-  $( '.date-picker' ).each(function() {
-
-    const field = $( this ).find('.date-picker__field');
-
-    field.on('click', function(event) {
-      $( this ).parent().find('.date-picker__calendar').toggle();
-    });
-
-    connect(field, '.date-picker__calendar', field.find('.input'));
-  });
+  $( '.date-picker' ).toxinDatePicker();
 }
 
 $( document ).ready( setDatepicker );
