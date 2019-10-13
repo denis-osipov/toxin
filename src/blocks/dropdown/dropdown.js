81ff004e239a64ad72fea0de9e556e7b7e4e7444
Denis Osipov <osipov_d@list.ru>
2019-10-13 15:32:00 +0500

    Use new connect block in dropdown

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -15,7 +15,7 @@
 // };
 
 import './dependencies.js';
-const connect = require('blocksPath/connect/connect');
+const connect = require('blocksPath/connect/connect').connect;
 
 (function( $ ) {
   // Main method creating dropdown
