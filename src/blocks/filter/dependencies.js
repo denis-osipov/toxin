60da7921cf6be5c5a486d38ea67d66e9da1f7ce7
Denis Osipov <osipov_d@list.ru>
2019-10-14 18:31:58 +0500

    Add filter block

---
+++ src/blocks/filter/dependencies.js
@@ -0,0 +1,9 @@
+// File generated automatically.
+// Any changes will be discarded during next compilation.
+
+import '../date-picker/date-picker.js';
+import '../guests/guests.js';
+import '../slider/slider.js';
+import '../list/list.js';
+import '../dropdown/dropdown.js';
+import '../exp-list/exp-list.js';
