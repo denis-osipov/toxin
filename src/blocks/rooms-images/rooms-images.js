c7e284d2a830a6c9b9a87c722bedd5d96bf461c1
Denis Osipov <osipov_d@list.ru>
2019-10-15 12:13:25 +0500

    Split out images from rooms-info

---
+++ src/blocks/rooms-images/rooms-images.js
@@ -0,0 +1,6 @@
+const images = {
+  888: Array(4).fill(require('images/rooms/888.jpg')),
+  840: Array(4).fill(require('images/rooms/840.jpg'))
+};
+
+module.exports = images;
--- src/blocks/rooms-info/rooms-info.js
+++ src/blocks/rooms-info/rooms-info.js
@@ -3,26 +3,14 @@ const rooms = {
     type: 'Люкс',
     cost: '9 990₽',
     rating: 5,
-    comments: '145 Отзывов',
-    images: [
-      require('images/rooms/888_normal.jpg'),
-      require('images/rooms/888_normal.jpg'),
-      require('images/rooms/888_normal.jpg'),
-      require('images/rooms/888_normal.jpg')
-    ]
+    comments: '145 Отзывов'
   },
   840: {
     type: '',
     cost: '9 900₽',
     rating: 4,
-    comments: '65 Отзывов',
-    images: [
-      require('images/rooms/840_normal.jpg'),
-      require('images/rooms/840_normal.jpg'),
-      require('images/rooms/840_normal.jpg'),
-      require('images/rooms/840_normal.jpg')
-    ]
-    }
+    comments: '65 Отзывов'
+  }
 };
 
 module.exports = rooms;
