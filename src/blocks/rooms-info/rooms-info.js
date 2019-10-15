4305e1656fb2433d8b90f67177ed0068f2cbd054
Denis Osipov <osipov_d@list.ru>
2019-10-15 13:33:51 +0500

    Add info for more rooms

--- src/blocks/rooms-info/rooms-info.js
+++ src/blocks/rooms-info/rooms-info.js
@@ -10,6 +10,66 @@ const rooms = {
     cost: '9 900₽',
     rating: 4,
     comments: '65 Отзывов'
+  },
+  980: {
+    type: 'Люкс',
+    cost: '8 500₽',
+    rating: 3,
+    comments: '35 Отзывов'
+  },
+  856: {
+    type: '',
+    cost: '7 300₽',
+    rating: 5,
+    comments: '19 Отзывов'
+  },
+  740: {
+    type: '',
+    cost: '6 000₽',
+    rating: 4,
+    comments: '44 Отзывов'
+  },
+  982: {
+    type: '',
+    cost: '5 800₽',
+    rating: 6,
+    comments: '56 Отзывов'
+  },
+  678: {
+    type: '',
+    cost: '5 500₽',
+    rating: 5,
+    comments: '25 Отзывов'
+  },
+  450: {
+    type: '',
+    cost: '5 300₽',
+    rating: 4,
+    comments: '39 Отзывов'
+  },
+  350: {
+    type: '',
+    cost: '5 000₽',
+    rating: 3,
+    comments: '77 Отзывов'
+  },
+  666: {
+    type: '',
+    cost: '5 000₽',
+    rating: 5,
+    comments: '25 Отзывов'
+  },
+  444: {
+    type: '',
+    cost: '5 000₽',
+    rating: 3,
+    comments: '15 Отзывов'
+  },
+  352: {
+    type: '',
+    cost: '5 000₽',
+    rating: 3,
+    comments: '55 Отзывов'
   }
 };
 
