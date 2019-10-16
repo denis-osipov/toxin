5bfa5eb5563a71c724503b2c3a778cc271fe7714
Denis Osipov <osipov_d@list.ru>
2019-10-16 09:18:03 +0500

    Activate all entries for tests

--- webpack.config.js
+++ webpack.config.js
@@ -97,11 +97,11 @@ const pathToEntries = path.join(module.exports.context, 'pages');
 const types = ['.js', '.scss'];
 
 [
-  // 'colorsAndType',
-  // 'formElements',
-  // 'cards',
-  // 'headersAndFooters',
-  // 'landingPage',
+  'colorsAndType',
+  'formElements',
+  'cards',
+  'headersAndFooters',
+  'landingPage',
   'searchRoom'
 ].forEach(entryName => {
   const entryFiles = [];
