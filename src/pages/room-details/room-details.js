f3c7ac09a4defd8287672d29f0872d6664b262ee
Denis Osipov <osipov_d@list.ru>
2019-10-17 18:47:07 +0500

    Add new blocks to room-details

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -1,4 +1,6 @@
 // Automatically generated imports.
 // Any changes in this block will be discarded during next compilation.
 import '../toxin/toxin.js';
+import '../../blocks/list/list.js';
+import '../../blocks/comments/comments.js';
 // End of block with automatically generated imports.
