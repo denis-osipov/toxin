e504e9cbf7108e58341486c32a403c0e6b3ccd75
Denis Osipov <osipov_d@list.ru>
2019-10-17 12:04:47 +0500

    Add styles and script for PP in search-room

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -3,3 +3,17 @@
 import '../toxin/toxin.js';
 import '../../blocks/filter/filter.js';
 // End of block with automatically generated imports.
+
+$( document ).ready(function(jQery) {
+
+  const reservationDates = $( '.calendar__picker' ).data('datepicker');
+  reservationDates.selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+
+  const reservationGuests = $( '.dropdown_type_guests' ).data('dropdown');
+  reservationGuests.set([2, 1, 1]);
+
+  const conveniences = $( '.dropdown_type_conveniences' ).data('dropdown');
+  conveniences.set([2, 2]);
+
+  $( '.room-plate' ).first().addClass('room-plate_active');
+});
