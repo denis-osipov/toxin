8adbc94dded2435834a9f88f796828db85937129
Denis Osipov <osipov_d@list.ru>
2019-10-19 17:48:28 +0500

    Tune appearance for card

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -10,4 +10,10 @@ import '../../blocks/card/card.js';
 $( document ).ready(function(jQery) {
   const chart = $( '.pie-chart' ).data('chart');
   chart.setActive(1);
+
+  const reservationDates = $( '.calendar__picker' ).data('datepicker');
+  reservationDates.selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)]);
+
+  const reservationGuests = $( '.dropdown_type_guests' ).data('dropdown');
+  reservationGuests.set([2, 1, 0]);
 });
