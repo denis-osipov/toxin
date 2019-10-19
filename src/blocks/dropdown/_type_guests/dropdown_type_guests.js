0352b5b20ad494f433b3e6f691c33850725fe1ee
Denis Osipov <osipov_d@list.ru>
2019-10-19 15:30:14 +0500

    Use wording module in dropdown

--- src/blocks/dropdown/_type_guests/dropdown_type_guests.js
+++ src/blocks/dropdown/_type_guests/dropdown_type_guests.js
@@ -1,44 +1,14 @@
+const wording = require('../../../../utils/wording');
+
 function setGuests( jQuery ) {
   $( '.dropdown_type_guests' ).dropdown({
-    wording: (value) => {
-      if (value == 0) {
-        return 'Сколько гостей';
-      }
-      else if (value === 1 || (value % 100 !== 11 && value % 10 === 1)) {
-        return value + ' гость';
-      }
-      else if ((function() {
-        const remMain = value % 100;
-        const remSub = remMain % 10;
-        return remSub > 1 && remSub < 5 && !(remMain > 11 && remMain < 15);
-      })()) {
-        return value + ' гостя';
-      }
-      else {
-        return value + ' гостей';
-      }
-    },
+    wording: wording,
+    words: ['Сколько гостей', 'гость', 'гостя', 'гостей'],
     special: [
       {
         index: [2],
-        wording: (value) => {
-          if (value == 0) {
-            return '';
-          }
-          else if (value === 1 || (value % 100 !== 11 && value % 10 === 1)) {
-            return value + ' младенец';
-          }
-          else if ((function() {
-            const remMain = value % 100;
-            const remSub = remMain % 10;
-            return remSub > 1 && remSub < 5 && !(remMain > 11 && remMain < 15);
-          })()) {
-            return value + ' младенца';
-          }
-          else {
-            return value + ' младенцев';
-          }
-        }
+        wording: wording,
+        words: ['', 'младенец', 'младенца', 'младенцев']
       }
     ],
     control: '.control'
